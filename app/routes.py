18e03647fc210d71647d5269396824b96d748540
Ribeiro <cassianocarneiro@usp.br>
2021-03-18 20:03:34 +0000

    parte 1

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,12 @@
 from app import app
-from flask impor render_template
+from flask import render_template
 
 @app.route("/") # Para tal url, executar a função abaixo
 @app.route("/index") # Também para essa url, executar a função abaixo
 def index():
-    return render_template("index.html")
+    user = {'username': 'Mr. Cassiano'}
+    posts = [
+        {'author': {'username' : 'Maria'}, 'body': "Lancei o míssel."},
+        {'author': {'username' : 'Alexei'}, 'body': "Tá bom, Maria."}
+    ]
+    return render_template("index.html", user=user, posts=posts)
